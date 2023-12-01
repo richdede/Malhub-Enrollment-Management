3f4c48877014fe4108e6e96c49b586a787b2c706
richdede <deeprince2002@gmail.com>
2023-12-01 08:29:27 -0800

    course api crud

--- api-laravel/routes/api.php
+++ api-laravel/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\CourseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/auth/register', [LoginController::class, 'createUser']);
 Route::post('/auth/login', [LoginController::class, 'loginUser']);
+
+Route::apiResource('/course', CourseController::class);
