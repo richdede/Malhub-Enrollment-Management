a97b9d2c40c139211459dd3b9712c11482c50d3a
richdede <deeprince2002@gmail.com>
2023-12-09 09:08:17 -0800

    updated reg

--- client/src/components/Sides.jsx
+++ client/src/components/Sides.jsx
@@ -32,32 +32,6 @@ const Sides = () => {
     },
   ];
 
-    const sides = [
-        {
-            id: 1,
-            name: 'Dashboard',
-            path: '/sidebar/welcome',
-            icon: '../../Public/dashboards.png'
-        },
-        {
-            id: 2,
-            name: 'Courses',
-            path: '/sidebar/courses',
-            icon: '../../Public/homework.png'
-        },
-        {
-            id: 3,
-            name: 'Workspace',
-            path: '/sidebar/workspace',
-            icon: '../../Public/workplace.png'
-        },
-        {
-            id: 4,
-            name: 'Payment',
-            path: 'payment',
-            icon: '../../Public/transaction-history.png'
-        }
-    ]
 
     return (
         <div className='sides'>
